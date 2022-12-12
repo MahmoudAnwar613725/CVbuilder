8791d793ac62a82211680a2fc647d592f051ac8b
MahmoudAnwar613725 <mahmohamed@miu.edu>
2022-12-11 20:29:50 -0600

    add login and shared preference to the application

--- app/src/main/java/com/course/cvbuilder/ui/login/LoginActivity.kt
+++ app/src/main/java/com/course/cvbuilder/ui/login/LoginActivity.kt
@@ -1,18 +1,18 @@
 package com.course.cvbuilder.ui.login
 
- import android.content.Intent
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
- import com.course.cvbuilder.ui.main.MainActivity
- import com.course.cvbuilder.databinding.ActivityLoginBinding
+import com.course.cvbuilder.ui.main.MainActivity
+import com.course.cvbuilder.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-    private lateinit var prefs : SharedPreferences
+    private lateinit var prefs: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -23,8 +23,8 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun getSaveUser() {
-        val prefUser = prefs.getString("username","")
-        val prefPass = prefs.getString("password","")
+        val prefUser = prefs.getString("username", "")
+        val prefPass = prefs.getString("password", "")
         prefUser?.let { binding.etEmail.setText(it) }
         prefPass?.let { binding.etPassword.setText(it) }
     }
@@ -33,24 +33,26 @@ class LoginActivity : AppCompatActivity() {
         val user = binding.etEmail.text.toString().trim()
         val pass = binding.etPassword.text.toString().trim()
 
-        if(user.isEmpty()){
-            Toast.makeText(applicationContext,"Please enter your username", Toast.LENGTH_LONG).show()
+        if (user.isEmpty()) {
+            Toast.makeText(applicationContext, "Please enter your username", Toast.LENGTH_LONG)
+                .show()
             return
         }
-        if(pass.isEmpty()){
-            Toast.makeText(applicationContext,"Please enter your password", Toast.LENGTH_LONG).show()
+        if (pass.isEmpty()) {
+            Toast.makeText(applicationContext, "Please enter your password", Toast.LENGTH_LONG)
+                .show()
             return
         }
         openMainActivity(user, pass)
     }
 
-    private fun openMainActivity(user: String, pass: String){
+    private fun openMainActivity(user: String, pass: String) {
         with(prefs.edit()) {
             putString("username", user)
             putString("password", pass)
-             apply()
+            apply()
         }
-
+        finish()
         startActivity(Intent(this, MainActivity::class.java))
     }
 
--- app/src/main/java/com/course/cvbuilder/ui/main/contact/ContactFragment.kt
+++ app/src/main/java/com/course/cvbuilder/ui/main/contact/ContactFragment.kt
@@ -3,12 +3,12 @@ package com.course.cvbuilder.ui.main.contact
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.provider.ContactsContract
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.course.cvbuilder.R
 
 
@@ -27,35 +27,35 @@ class ContactFragment : Fragment() {
         // Inflate the layout for this fragment
         val v = inflater.inflate(R.layout.fragment_contact, container, false)
 
-        v.findViewById<LinearLayout>(R.id.phone).setOnClickListener{
+        v.findViewById<LinearLayout>(R.id.phone).setOnClickListener {
             makeCall()
         }
-        v.findViewById<LinearLayout>(R.id.linkedIn).setOnClickListener{
+        v.findViewById<LinearLayout>(R.id.linkedIn).setOnClickListener {
             browseWebsite("https://www.linkedin.com/in/mahmoud-anwar-fadel/")
         }
 
-        v.findViewById<LinearLayout>(R.id.github).setOnClickListener{
+        v.findViewById<LinearLayout>(R.id.github).setOnClickListener {
             browseWebsite("https://github.com/MahmoudAnwar613725")
         }
-        v.findViewById<LinearLayout>(R.id.email).setOnClickListener{
+        v.findViewById<LinearLayout>(R.id.email).setOnClickListener {
             sendEmail()
-         }
+        }
         return v
     }
 
-    fun onContactClick(view: View){
-        when(view.id){
-            R.id.phone->{
+    fun onContactClick(view: View) {
+        when (view.id) {
+            R.id.phone -> {
 
             }
-            R.id.linkedIn->{
+            R.id.linkedIn -> {
 
                 browseWebsite("https://www.linkedin.com/in/mahmoud-anwar-fadel/")
             }
-            R.id.github->{
+            R.id.github -> {
                 browseWebsite("https://github.com/MahmoudAnwar613725")
             }
-            R.id.email->{
+            R.id.email -> {
                 sendEmail()
             }
         }
@@ -70,13 +70,22 @@ class ContactFragment : Fragment() {
     }
 
 
-
     private fun sendEmail() {
-        val intent = Intent(Intent.ACTION_SENDTO)
-        intent.data = Uri.parse("mailto:")
-        intent.putExtra(Intent.EXTRA_EMAIL, "mhmodu@muy.edu")
-        intent.putExtra(Intent.EXTRA_SUBJECT, "open vacancy")
-        startActivity(intent)
+
+        val packageManager = activity?.packageManager
+        if (packageManager != null) {
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:")
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf<String>("mahmouhamed@miu.edu"))
+            intent.putExtra(Intent.EXTRA_SUBJECT, "open vacancy")
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            }
+
+        }else{
+            Toast.makeText(activity, "no app to send", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     private fun browseWebsite(website: String) {
